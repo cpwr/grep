8987935f55e2b20f3c296d23fdeda35626d638c5
cpwr <k.degtyarenko@gmail.com>
2015-07-12 21:40:27 +0300

    Added some code

--- app.py
+++ app.py
@@ -1,1 +1,24 @@
-__author__ = 'newbie'
+import argparse
+import os
+import fnmatch
+
+
+def grep(pattern, keyword):
+    for root, dirs, files in os.walk("."):
+        for item in fnmatch.filter(files, pattern):
+            with open(item, 'r') as f:
+                for number, line in enumerate(f, 1):
+                    if str(keyword) in line:
+                        l = line.split(keyword)
+                        print(
+                            os.path.join('', str(item)) + ":",
+                            number,
+                            l[0] + '\x1b[31m %s\x1b[0m' % keyword + l[1]
+                        )
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('pattern')
+    parser.add_argument('keyword')
+    args = parser.parse_args()
+    grep(args.pattern, args.keyword)
